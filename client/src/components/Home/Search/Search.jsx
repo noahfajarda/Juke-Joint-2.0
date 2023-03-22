0ffb0637b4ddee9dc1edef4da570c0f39bc6c73d
Noah Fajarda <noahfajarda@gmail.com>
2023-03-22 03:56:12 -0700

    a

--- client/src/components/Home/Search/Search.jsx
+++ client/src/components/Home/Search/Search.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 // data retrieval
-import fetchTrackData from "../../../utils/dataRetrieval/fetchTrackData";
+import retrieveData from "../../../utils/dataRetrieval/fetchTrackData";
 
 export default function Search({ setId, setType, type }) {
   // set form state variables, change, submit
   const [form, setForm] = useState({
     search: "",
+    type: "track",
   });
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -22,19 +23,24 @@ export default function Search({ setId, setType, type }) {
     // maybe reset the form
     // setForm({ search: "" });
 
-    // filtered track data already
-    // refer to function to view all data retrieved
-    const trackData = await fetchTrackData(form.search);
-    console.log(trackData);
+    if (form.type === "track") {
+      // filtered track data already
+      // refer to function to view all data retrieved
+      const trackData = await retrieveData.fetchTrackData(form.search);
+      console.log(trackData);
 
-    // set the widget
-    setType(type);
-    setId(trackData.trackId);
+      // set the widget
+      setType(type);
+      setId(trackData.trackId);
+    } else if (form.type === "artist") {
+      console.log("do the search artist function");
+    }
 
     // display the widget
     const widgetContainerEl = document.querySelector(".spotify-widget");
     widgetContainerEl.style.display = "block";
   };
+  const types = ["track", "artist", "album"];
 
   return (
     <form
@@ -52,6 +58,17 @@ export default function Search({ setId, setType, type }) {
         value={form.search}
         onChange={handleChange}
       />
+
+      <select id="type" name="type" value={form.type} onChange={handleChange}>
+        {types.map((type) => {
+          return (
+            <option value={type} key={type}>
+              {type}
+            </option>
+          );
+        })}
+      </select>
+
       <button type="submit" id="submit" className="submit">
         Search
       </button>
--- client/src/utils/dataRetrieval/fetchTrackData.js
+++ client/src/utils/dataRetrieval/fetchTrackData.js
@@ -1,7 +1,7 @@
 import access from "../retrieve-authorization-token"
 import dataRetrieval from "../fetch-API-data-&-colors"
 
-export default async function fetchTrackData(track) {
+async function fetchTrackData(track) {
   // retrieve access token
   const token = await access.getAccessToken();
 
@@ -27,3 +27,32 @@ export default async function fetchTrackData(track) {
 
   return trackDBDataToInsert;
 }
+
+async function fetchAlbumData(album) {
+  // retrieve access token
+  const token = await access.getAccessToken();
+
+  // hit API for track to retrieve data & filter
+  // pass in TOKEN & TRACK
+  const test = await dataRetrieval.search_for_album(token, album)
+  let data = await test.json();
+  // data = data.tracks.items[0];
+
+  // this is all API data retrieved
+  console.log(data)
+
+
+  // // isolate all the data you need for the DB
+  // const trackDBDataToInsert = {
+  //   trackName: data.name,
+  //   trackArtist: data.artists[0].name,
+  //   trackId: data.id,
+  //   title: "Track",
+  //   trackArt: data.album.images[1].url
+  //   // for track art: 1=large, 2=medium, 3=small
+  // }
+
+  // return trackDBDataToInsert;
+}
+
+export default { fetchTrackData, fetchAlbumData }
